a2f1dd8f5c3b7e774314a049448fa00454b13bf7
dpwebadmin <Analyn@Analyns-MacBook-Pro.local>
2015-04-19 18:17:17 -0400

    Added Penn Ten - Denzel Cummings v.1

---
+++ PennTen/denzelcummings/assets/pennten_navbar/pennten_navbar_edge.js
@@ -0,0 +1,157 @@
+
+(function(compId){"use strict";var _=null,y=true,n=false,x2='5.0.0.375',x80='303px',x14='rgb(0, 0, 0)',x19='-1071px',x18='home',x109='103px',e6='${back_button}',cl='clip',x105='-477px',x30='rgba(119,119,119,0.22)',x100='-2996px',x10='238px',x3='rgba(28,28,28,0.00)',x7='rect(@@0@@px @@1@@px @@2@@px @@3@@px)',x96='shadow',x83='hitCopy6',x78='245px',x38='rectangle',x92='-374px',e16='${Rectangle}',x66='553px',x58='-594px',xc='rgba(0,0,0,1)',x74='129px',x40='rectangle2',x13='Rectangle',x59='-2375px',x52='rectangle9',x55='522px',x27='63px',x36='0',x54='rectangle10',x53='464px',x21='2380px',x46='rectangle5',o='opacity',x57='names',x110='106px',x45='232px',x31='62px',x65='38px',x51='406px',x90='13px',x85='hitCopy5',x81='hitCopy7',x20='-211px',x75='hitCopy4',x44='rectangle4',x37='57',x56='rectangle11',x69='71px',x67='photos',x107='1060px',x71='hitCopy2',x89='hitCopy9',x112='160px',x94='1230px',x77='hitCopy3',x63='16px',x118='103',x113='back',x119='106',x29='579px',x48='rectangle6',bg='background-color',x70='31px',x12='auto',x64='11px',x15='rgba(48,48,48,0.32)',x60='1480px',x22='470px',zy='scaleY',x103='655px',x23='rgba(0,0,0,0)',x11='57px',x25='47px',zx='scaleX',x41='116px',x104='pennten_logo',x26='background',x117='-21px',x42='rectangle3',x33='579',x101='shadowCopy2',x50='rectangle8',x116='pennten_button',x95='6540px',x73='rgba(42,0,0,0.00)',m='rect',x9='0px',x91='hit',x111='-72px',x1='5.0.0',x32='238',x86='477px',x76='187px',x82='361px',d='display',x102='365px',e8='${body_button}',i='none',x99='-618px',x17='0.1',x120='64px',x79='hitCopy8',x88='535px',x28='54px',x98='180',x106='1030px',x115='0.6',x39='58px',b='block',e4='${pennten_button2}',x61='5330px',x87='hitCopy10',x34='body',x93='-2997px',g='image',x43='174px',x84='419px',e121='${pennten_button}',x72='pointer',x35='group',x49='348px',e5='${home_button}',x47='290px';var g24='home.svg',g62='names.svg',g68='photos.png',g108='pennten_logo.svg',g114='back.svg',g97='shadow.svg';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{id:'body_button',symbolName:'body_button',v:i,t:m,r:['-62','-5px','365','655','auto','auto'],cl:'rect(0px 365px 655px 0px)'},{id:'home_button',symbolName:'home',v:i,t:m,r:['0','0px','238','47','auto','auto'],cl:'rect(0px 238px 0px 0px)',tf:[[],[],[],['1','1.02128']]},{id:'pennten_button2',symbolName:'pennten_button2',v:b,t:m,r:['88','18','62','64','auto','auto'],o:0},{id:'back_button',symbolName:'back_button',v:i,t:m,r:['0','628','238','16','auto','auto'],cl:'rect(0px 238px 0px 0px)',cu:'pointer'}],style:{'${Stage}':{isStage:true,r:['null','null','238px','643px','auto','auto'],overflow:'hidden',f:[x3]}}},tt:{d:4114,a:y,l:{"click":1000,"back":2618},data:[["eid85",o,250,250,"swing",e4,'0','1'],["eid47",d,1000,0,"swing",e5,i,b],["eid56",cl,2250,204,"swing",e6,[0,238,0,0],[0,238,16,0],{vt:x7}],["eid58",cl,2618,204,"swing",e6,[0,238,16,0],[0,238,0,0],{vt:x7}],["eid41",cl,1163,1150,"swing",e8,[0,365,0,0],[0,365,635,0],{vt:x7}],["eid52",cl,2801,1150,"swing",e8,[0,365,635,0],[0,365,0,0],{vt:x7}],["eid48",d,1163,0,"swing",e8,i,b],["eid53",d,3951,0,"swing",e8,b,b],["eid83",d,1000,0,"swing",e4,b,i],["eid59",d,2250,0,"linear",e6,i,b],["eid39",cl,1000,144,"swing",e5,[0,238,0,0],[0,238,47,0],{vt:x7}],["eid51",cl,3606,144,"swing",e5,[0,238,47,0],[0,238,0,0],{vt:x7}]]}},"rectangle":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{r:[x9,x9,x10,x11,x12,x12],id:x13,s:[0,x14,i],t:m,f:[x15]}],style:{'${symbolSelector}':{r:[_,_,x10,x11]}}},tt:{d:566,a:n,l:{"rollover":75,"rollout":456},data:[["eid34",bg,75,268,"linear",e16,'rgba(48,48,48,0.32)','rgba(241,241,241,0.4)'],["eid35",bg,456,110,"linear",e16,'rgba(241,241,241,0.4)','rgba(52,52,52,0.40)']]}},"home":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{tf:[[],[],[],[x17,x17]],id:x18,t:g,r:[x19,x20,x21,x22,x12,x12],f:[x23,im+g24,x9,x9]}],style:{'${symbolSelector}':{r:[_,_,x10,x25]}}},tt:{d:0,a:n,data:[]}},"body_button":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x26,s:[0,xc,i],r:[x27,x28,x10,x29,x12,x12],f:[x30]},{r:[x31,x28,x32,x33,x12,x12],id:x34,t:x35,c:[{r:[x36,x9,x32,x37,x12,x12],id:x38,sN:x38,t:m},{r:[x36,x39,x32,x37,x12,x12],id:x40,sN:x38,t:m},{r:[x36,x41,x32,x37,x12,x12],id:x42,sN:x38,t:m},{r:[x36,x43,x32,x37,x12,x12],id:x44,sN:x38,t:m},{r:[x36,x45,x32,x37,x12,x12],id:x46,sN:x38,t:m},{r:[x36,x47,x32,x37,x12,x12],id:x48,sN:x38,t:m},{r:[x36,x49,x32,x37,x12,x12],id:x50,sN:x38,t:m},{r:[x36,x51,x32,x37,x12,x12],id:x52,sN:x38,t:m},{r:[x36,x53,x32,x37,x12,x12],id:x54,sN:x38,t:m},{r:[x36,x55,x32,x37,x12,x12],id:x56,sN:x38,t:m},{tf:[[],[],[],[x17,x17]],id:x57,t:g,r:[x58,x59,x60,x61,x12,x12],f:[x23,im+g62,x9,x9]},{r:[x63,x64,x65,x66,x12,x12],id:x67,t:g,f:[x23,im+g68,x9,x9]},{t:m,r:[x9,x69,x10,x70,x12,x12],id:x71,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x74,x10,x70,x12,x12],id:x75,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x76,x10,x70,x12,x12],id:x77,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x78,x10,x70,x12,x12],id:x79,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x80,x10,x70,x12,x12],id:x81,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x82,x10,x70,x12,x12],id:x83,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x84,x10,x70,x12,x12],id:x85,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x86,x10,x70,x12,x12],id:x87,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x88,x10,x70,x12,x12],id:x89,s:[0,x14,i],cu:x72,f:[x73]},{t:m,r:[x9,x90,x10,x70,x12,x12],id:x91,s:[0,x14,i],cu:x72,f:[x73]},{tf:[[],[],[],[x17,x17]],r:[x92,x93,x94,x95,x12,x12],id:x96,o:0.19,t:g,f:[x23,im+g97,x9,x9]},{tf:[[],[x98],[],[x17,x17]],r:[x99,x100,x94,x95,x12,x12],id:x101,o:0.49,t:g,f:[x23,im+g97,x9,x9]}]}],style:{'${symbolSelector}':{r:[_,_,x102,x103]}}},tt:{d:0,a:n,data:[]}},"pennten_button":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{tf:[[],[],[],[x17,x17]],id:x104,t:g,r:[x105,x105,x106,x107,x12,x12],f:[x23,im+g108,x9,x9]}],style:{'${symbolSelector}':{r:[_,_,x109,x110]}}},tt:{d:1874,a:n,l:{"rollover":326,"rollout":1874},data:[]}},"back_button":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{r:[x19,x111,x21,x112,x12,x12],id:x113,tf:[[],[],[],[x17,x17]],t:g,f:[x23,im+g114,x9,x9]}],style:{'${symbolSelector}':{r:[_,_,x10,x63]}}},tt:{d:0,a:n,data:[]}},"pennten_button2":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{tf:[[],[],[],[x115,x115]],t:m,id:x116,v:b,sN:x116,cu:x72,r:[x117,x117,x118,x119,x12,x12]}],style:{'${symbolSelector}':{r:[_,_,x31,x120]}}},tt:{d:1000,a:n,l:{"rollover":190,"rollout":657},data:[["eid79",zy,190,211,"swing",e121,'0.6','0.83'],["eid80",zy,657,211,"swing",e121,'0.83','0.6'],["eid50",d,1000,0,"swing",e121,b,i],["eid78",zx,190,211,"swing",e121,'0.6','0.83'],["eid81",zx,657,211,"swing",e121,'0.83','0.6']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-144874321");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2454,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4114,function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${back_button}","click",function(sym,e){sym.play("back");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${pennten_button2}","click",function(sym,e){sym.play("click");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",500,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'rectangle'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",75,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",343,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",343,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",456,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",566,function(sym,e){sym.stop();});
+//Edge binding end
+})("rectangle");
+//Edge symbol end:'rectangle'
+
+//=========================================================
+
+//Edge symbol: 'home'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${home}","click",function(sym,e){});
+//Edge binding end
+})("home");
+//Edge symbol end:'home'
+
+//=========================================================
+
+//Edge symbol: 'body_button'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${hitCopy9}","mouseenter",function(sym,e){sym.getSymbol("rectangle11").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy9}","mouseleave",function(sym,e){sym.getSymbol("rectangle11").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy9}","mouseout",function(sym,e){sym.getSymbol("rectangle11").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy10}","mouseenter",function(sym,e){sym.getSymbol("rectangle10").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy10}","mouseleave",function(sym,e){sym.getSymbol("rectangle10").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy10}","mouseout",function(sym,e){sym.getSymbol("rectangle10").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy5}","mouseenter",function(sym,e){sym.getSymbol("rectangle9").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy5}","mouseleave",function(sym,e){sym.getSymbol("rectangle9").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy5}","mouseout",function(sym,e){sym.getSymbol("rectangle9").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy6}","mouseenter",function(sym,e){sym.getSymbol("rectangle8").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy6}","mouseleave",function(sym,e){sym.getSymbol("rectangle8").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy6}","mouseout",function(sym,e){sym.getSymbol("rectangle8").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy7}","mouseenter",function(sym,e){sym.getSymbol("rectangle6").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy7}","mouseleave",function(sym,e){sym.getSymbol("rectangle6").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy7}","mouseout",function(sym,e){sym.getSymbol("rectangle6").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy8}","mouseenter",function(sym,e){sym.getSymbol("rectangle5").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy8}","mouseleave",function(sym,e){sym.getSymbol("rectangle5").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy8}","mouseout",function(sym,e){sym.getSymbol("rectangle5").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy3}","mouseenter",function(sym,e){sym.getSymbol("rectangle4").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy3}","mouseleave",function(sym,e){sym.getSymbol("rectangle4").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy3}","mouseout",function(sym,e){sym.getSymbol("rectangle4").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy4}","mouseenter",function(sym,e){sym.getSymbol("rectangle3").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy4}","mouseleave",function(sym,e){sym.getSymbol("rectangle3").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy4}","mouseout",function(sym,e){sym.getSymbol("rectangle3").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy2}","mouseenter",function(sym,e){sym.getSymbol("rectangle2").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy2}","mouseleave",function(sym,e){sym.getSymbol("rectangle2").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy2}","mouseout",function(sym,e){sym.getSymbol("rectangle2").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hit}","mouseenter",function(sym,e){sym.getSymbol("rectangle").play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hit}","mouseleave",function(sym,e){sym.getSymbol("rectangle").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hit}","mouseout",function(sym,e){sym.getSymbol("rectangle").play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy9}","click",function(sym,e){window.open("http://thedp.com/pennten/christopheryao","Christopher Yao");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy2}","click",function(sym,e){window.open("http://thedp.com/pennten/allysonahlstrom","Allyson Ahlstrom");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy4}","click",function(sym,e){window.open("http://thedp.com/pennten/denzelcummings","Denzel Cummings");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy3}","click",function(sym,e){window.open("http://thedp.com/pennten/danielfine","Daniel Fine");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy8}","click",function(sym,e){window.open("http://thedp.com/pennten/shadrackfrimpong","Shadrack Frimpong");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy7}","click",function(sym,e){window.open("http://thedp.com/pennten/arielkoren","Ariel Koren");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy6}","click",function(sym,e){window.open("http://thedp.com/pennten/adrianlievano","Adrian Lievano");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy5}","click",function(sym,e){window.open("http://thedp.com/pennten/taylormclendon","Taylor McLendon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hitCopy10}","click",function(sym,e){window.open("http://thedp.com/pennten/seaonshin","Seaon Shin");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${hit}","click",function(sym,e){window.open("http://thedp.com/pennten/sayidabdullaev","Sayid Abdullaev");});
+//Edge binding end
+})("body_button");
+//Edge symbol end:'body_button'
+
+//=========================================================
+
+//Edge symbol: 'pennten_button'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",326,function(sym,e){});
+//Edge binding end
+})("pennten_button");
+//Edge symbol end:'pennten_button'
+
+//=========================================================
+
+//Edge symbol: 'back_button'
+(function(symbolName){})("back_button");
+//Edge symbol end:'back_button'
+
+//=========================================================
+
+//Edge symbol: 'pennten_button2'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",867,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",401,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${pennten_button}","mouseenter",function(sym,e){sym.play("rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${pennten_button}","mouseout",function(sym,e){sym.play("rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${pennten_button}","mouseleave",function(sym,e){sym.play("rollout");});
+//Edge binding end
+})("pennten_button2");
+//Edge symbol end:'pennten_button2'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-144874321");
