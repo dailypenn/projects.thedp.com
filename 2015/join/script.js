073dd59494abd77fe6ad8b5568a510cd1af23a01
Andrew Fischer <afischer15@mac.com>
2016-01-10 12:47:49 -0500

    Content all added

--- 2015/join/script.js
+++ 2015/join/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
   $('#fullpage').fullpage({
-    anchors: ['intro', 'editorial', '3rdPage'],
+    anchors: ['intro', 'editorial', 'news', 'copy', 'sports'],
     sectionsColor: ['#EAE9E9', '#EAE9E9', '#7E8F7C'],
     css3: true,
     scrollingSpeed: 500,
