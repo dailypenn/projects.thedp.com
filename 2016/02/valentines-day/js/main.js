74694c4d6dada22f7c471a62d716f49d19da61e0
Andrew Fischer <afischer15@mac.com>
2016-02-13 20:47:01 -0500

    Changes and Stuff, waiting on graphics

--- 2016/02/valentines-day/js/main.js
+++ 2016/02/valentines-day/js/main.js
@@ -32,7 +32,7 @@
 			}
 		},
 		gridEl = document.getElementById('theGrid'),
-		sidebarEl = document.getElementById('theSidebar'),
+		// sidebarEl = document.getElementById('theSidebar'),
 		gridItemsContainer = gridEl.querySelector('section.grid'),
 		contentItemsContainer = gridEl.querySelector('section.content'),
 		gridItems = gridItemsContainer.querySelectorAll('.grid__item'),
