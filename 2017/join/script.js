af45445079d3cb9269556ebb3e9518f1bec987d0
Andrew Fischer <afischer15@mac.com>
2017-01-12 22:30:52 -0500

    Join site 2017

---
+++ 2017/join/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$("#demosMenu").change(function(){
+	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
+	});
+  $('#fullpage').fullpage({
+    anchors: ['join-intro', 'join-news', 'join-copy', 'join-sports', 'join-social', 'join-photo', 'join-design', 'join-video', 'join-web', 'join-ed-analytics', 'join-advertising', 'join-marketing', 'join-circulation', 'join-biz-analytics', 'join-34st', 'join-utb', 'join-join'],
+    css3: true,
+    scrollingSpeed: 500,
+    scrollBar: true,
+		autoScrolling: false//,
+//		fitToSection: false
+  });
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
