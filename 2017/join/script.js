3175eecbe188e351db5578c736668fd9d420c7ea
Andrew Fischer <afischer15@mac.com>
2017-01-13 14:53:58 -0500

    remove podcast

--- 2017/join/script.js
+++ 2017/join/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
   $('#fullpage').fullpage({
-    anchors: ['join-intro', 'join-news', 'join-copy', 'join-sports', 'join-social', 'join-photo', 'join-design', 'join-video', 'join-web', 'join-podcasts', 'join-ed-analytics', 'join-advertising', 'join-marketing', 'join-circulation', 'join-biz-analytics', 'join-34st', 'join-utb', 'join-join'],
+    anchors: ['join-intro', 'join-news', 'join-copy', 'join-sports', 'join-social', 'join-photo', 'join-design', 'join-video', 'join-web', /*'join-podcasts',*/ 'join-ed-analytics', 'join-advertising', 'join-marketing', 'join-circulation', 'join-biz-analytics', 'join-34st', 'join-utb', 'join-join'],
     css3: true,
     scrollingSpeed: 500,
     scrollBar: true,
