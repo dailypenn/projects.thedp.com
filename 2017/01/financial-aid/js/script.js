e709ebb29d56dcdb4a65572905cfb5230f5bab43
Alex Graves <graves@thedp.com>
2017-01-21 18:12:43 -0500

    update news blurbs

--- 2017/01/financial-aid/js/script.js
+++ 2017/01/financial-aid/js/script.js
@@ -13,17 +13,17 @@ var aidAppsChart = c3.generate({
   bindto: '#aidApps',
   data: {
     columns: [
-      ['Percent receiving Grant', 88.5, 86.1, 87.2, 90.4, 90.5, 91.1, 91.6, 91.4, 92.3, 91.9],
+      ['Percent receiving grant', 88.5, 86.1, 87.2, 90.4, 90.5, 91.1, 91.6, 91.4, 92.3, 91.9],
     ],
     colors: {
-      'Percent receiving Grant': '#5d9f7f'
+      'Percent receiving grant': '#5d9f7f'
     },
   },
   axis: {
     x: {
       label: {
-        text: 'Percent receiving Grant',
-        position: 'outer-center'
+        text: 'Percent receiving grant',
+        position: 'inner-right'
       },
       type: 'category',
       categories: ['2007-08', '2008-09', '2009-10', '2010-11', '2011-12', '2012-13', '2013-14', '2014-15', '2015-16', '2016-17'],
@@ -35,8 +35,8 @@ var aidAppsChart = c3.generate({
     },
     y: {
       label: {
-        text: 'Percentage of Applicants',
-        position: 'outer-middle'
+        text: 'Percentage of applicants',
+        position: 'inner-top'
       }
     }
   }
@@ -46,19 +46,19 @@ var aidreceiptsChart = c3.generate({
   bindto: '#aidreceipts',
   data: {
     columns: [
-      ['Aid Applicant Total', 4473, 4727, 5029, 5060, 5217, 5346, 5383, 5396, 5368, 5165],
-      ['Aid Recipient Total', 3959, 4071, 4385, 4576, 4719, 4870, 4933, 4933, 4954, 4749]
+      ['Aid applicant total', 4473, 4727, 5029, 5060, 5217, 5346, 5383, 5396, 5368, 5165],
+      ['Aid recipient total', 3959, 4071, 4385, 4576, 4719, 4870, 4933, 4933, 4954, 4749]
     ],
     colors: {
-      'Aid Applicant Total': '#5d9f7f',
-      'Aid Recipient Total': '#9ebdaa'
+      'Aid applicant total': '#5d9f7f',
+      'Aid recipient total': '#9ebdaa'
     },
   },
   axis: {
     x: {
       label: {
         text: 'Years',
-        position: 'outer-center'
+        position: 'inner-right'
       },
       type: 'category',
       categories: ['2007-08', '2008-09', '2009-10', '2010-11', '2011-12', '2012-13', '2013-14', '2014-15', '2015-16', '2016-17'],
@@ -71,7 +71,7 @@ var aidreceiptsChart = c3.generate({
     y: {
       label: {
         text: 'Students',
-        position: 'outer-middle'
+        position: 'inner-top'
       }
     }
   }
@@ -81,7 +81,7 @@ var gradAid = c3.generate({
   bindto: '#gradAid',
   data: {
     columns: [
-      ['Job', 845, 2372, 1787, 0, 1962, 1558, 0, 0, 2279, 0, 0, 0, 0, 0, 1393, 1132, 1476, 2178, 919, 1862],
+      ['Job/Work Study', 845, 2372, 1787, 0, 1962, 1558, 0, 0, 2279, 0, 0, 0, 0, 0, 1393, 1132, 1476, 2178, 919, 1862],
       ['Loan', 46470, 38498, 39870, 66917, 37895, 26905, 50847, 28747, 35423, 20462, 20282, 14622, 15620, 29803, 75862, 34604, 53703, 53950, 17432, 33180],
       ['Grant', 30354, 9506, 10268, 14469, 9877, 7377, 23267, 13507, 3535, 29497, 27201, 34495, 31213, 31760, 24610, 35234, 17593, 8751, 30258, 26618]
     ],
@@ -89,10 +89,10 @@ var gradAid = c3.generate({
     colors: {
       Grant: '#5d9f7f',
       Loan: '#9ebdaa',
-      Job: '#eedfa2'
+      'Job/Work Study': '#eedfa2'
     },
     groups: [
-      ['Job', 'Loan', 'Grant']
+      ['Job/Work Study', 'Loan', 'Grant']
     ]
   },
   axis: {
