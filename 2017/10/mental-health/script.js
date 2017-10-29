251ab8b267dd38ec726f34aea88cde00bacd2392
Brady Africk <bradyafrick@gmail.com>
2017-10-29 13:34:11 -0400

    made transition colors the same as color scheme

--- 2017/10/mental-health/script.js
+++ 2017/10/mental-health/script.js
@@ -239,7 +239,7 @@ $(document).ready(function() {
     // the layers are the elements that move from the sides
     nmbLayers : 3,
     // bg color of each layer
-    bgcolor : ['#708090', '#36454f', '#708090'],
+    bgcolor : ['#36454f', '#708090', '#36454f'],
     // effect classname
     effect : 'anim--effect-3',
     onStart : function(direction) {
