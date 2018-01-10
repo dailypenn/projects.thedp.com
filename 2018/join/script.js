1c8ae5c3c65c1d12fbf6da974628eab0ad7fab41
Alex Graves <graves@thedp.com>
2018-01-09 19:00:05 -0500

    add more department copy and photos

--- 2018/join/script.js
+++ 2018/join/script.js
@@ -20,6 +20,45 @@ for (var i = 0; i < edTabs.length; i++) {
     inactiveEd();
     this.classList.add('active');
     document.getElementById(this.id + '-text').classList.add('active');
+
+    // change department images
+    switch (this.id) {
+      case 'news':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.8), rgba(170, 30, 34, 0.8)), url('img/news.jpg')";
+        break;
+      case 'sports':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.9), rgba(170, 30, 34, 0.9)), url('img/sports.jpg')";
+        break;
+      case 'opinion':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.9), rgba(170, 30, 34, 0.9)), url('img/opinion.jpg')";
+        break;
+      case 'design':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.9), rgba(170, 30, 34, 0.9)), url('img/design.jpg')";
+        break;
+      case 'photo':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.8), rgba(170, 30, 34, 0.8)), url('img/photo.jpg')";
+        break;
+      case 'video':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.8), rgba(170, 30, 34, 0.8)), url('img/video.gif')";
+        break;
+      case 'copy':
+        document.querySelector('section.editorial').style.background =
+          "linear-gradient(rgba(170, 30, 34, 0.8), rgba(170, 30, 34, 0.8)), url('img/copy.jpg')";
+        break;
+      default:
+        document.querySelector('section.editorial').style.background = '#AA1E22';
+    }
+
+    // make sure image covers background
+    document.querySelector('section.editorial').style.backgroundSize = 'cover';
+    document.querySelector('section.editorial').style.backgroundPosition = 'center';
+    document.querySelector('section.editorial').style.backgroundRepeat = 'no-repeat';
   });
 }
 
@@ -45,5 +84,20 @@ for (var i = 0; i < edTabs.length; i++) {
     inactiveBiz();
     this.classList.add('active');
     document.getElementById(this.id + '-text').classList.add('active');
+
+    // change department images
+    switch (this.id) {
+      case 'web':
+        document.querySelector('section.business').style.background =
+          "linear-gradient(rgba(238, 238, 238, 0.8), rgba(238, 238, 238, 0.8)), url('img/web.jpg')";
+        break;
+      default:
+        document.querySelector('section.business').style.background = '#EEE';
+    }
+
+    // make sure image covers background
+    document.querySelector('section.business').style.backgroundSize = 'cover';
+    document.querySelector('section.business').style.backgroundPosition = 'center';
+    document.querySelector('section.business').style.backgroundRepeat = 'no-repeat';
   });
 }
