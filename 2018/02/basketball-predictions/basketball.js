3297accc8c755284486e01b0f3b95e89228920b3
jtomli <jtomli@seas.upenn.edu>
2018-02-20 17:51:54 -0500

    added mens records

---
+++ 2018/02/basketball-predictions/basketball.js
@@ -0,0 +1,34 @@
+var mensRecords = {
+	"penn": {
+		"wins": 9,
+		"losses": 1
+	},
+  "harvard": {
+    "wins": 9,
+		"losses": 1
+  },
+  "yale": {
+    "wins": 5,
+		"losses": 5
+  },
+  "brown": {
+    "wins": 4,
+		"losses": 6
+  },
+  "cornell": {
+    "wins": 4,
+		"losses": 6
+  },
+  "columbia": {
+    "wins": 4,
+		"losses": 6
+  },
+  "princeton": {
+    "wins": 3,
+		"losses": 7
+  },
+  "dartmouth": {
+    "wins": 2,
+		"losses": 8
+  },
+}
