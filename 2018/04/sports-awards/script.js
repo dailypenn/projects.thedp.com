0146a49f3b8173553b360b0614af5a09af07e154
Ishaan Rao <ishdr08@gmail.com>
2018-04-22 23:04:57 -0400

    finished eotya

--- 2018/04/sports-awards/script.js
+++ 2018/04/sports-awards/script.js
@@ -24,19 +24,25 @@ for (var i = 0; i < teamTabs.length; i++) {
     // change department images
     switch (this.id) {
       case 'team':
-        document.querySelector('section.team').style.background =
-          "linear-gradient(rgba(170, 30, 34, 0.8), rgba(170, 30, 34, 0.8)), url('https://snworksceo.imgix.net/dpn/917417c6-2a39-487a-bd75-8913f3427445.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/f5360b8d-c2c2-4310-9d7c-46913b65ac2d.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundColor = "#990000";
+        document.querySelector('section.team').style.backgroundBlendMode = "soft-light";
         break;
       case 'champ':
-        document.querySelector('section.team').style.background =
-          "linear-gradient(rgba(170, 30, 34, 0.9), rgba(170, 30, 34, 0.9)), url('https://snworksceo.imgix.net/dpn/01fd6964-dab3-4ec3-95e5-871ab1932e34.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/043f5740-2763-42f0-99a2-9c74980d288f.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundColor = "#990000";
+        document.querySelector('section.team').style.backgroundBlendMode = "soft-light";
         break;
       case 'coach':
-        document.querySelector('section.team').style.background =
-          "linear-gradient(rgba(170, 30, 34, 0.9), rgba(170, 30, 34, 0.9)), url('https://snworksceo.imgix.net/dpn/63f97845-7cb5-49eb-8322-bca5667583bc.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/1e437403-02a8-47ce-90cb-c2226e45b603.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundColor = "#990000";
+        document.querySelector('section.team').style.backgroundBlendMode = "soft-light";
         break;
       default:
-        document.querySelector('section.team').style.background = '#AA1E22';
+        document.querySelector('section.team').style.background = '#990000';
     }
 
     // make sure image covers background
@@ -72,20 +78,58 @@ for (var i = 0; i < indTabs.length; i++) {
     // change department images
     switch (this.id) {
       case 'maleath':
-        document.querySelector('section.individual').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.9), rgba(238, 238, 238, 0.9)), url('https://snworksceo.imgix.net/dpn/5803b1e8-72a6-4810-9770-60123f4a066d.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/3211d02f-bad8-499c-a3f3-d499c2f44732.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B";  
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
         break;
       case 'malerook':
-        document.querySelector('section.individual').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.8), rgba(238, 238, 238, 0.8)), url('https://snworksceo.imgix.net/dpn/7f265244-68c1-4483-b138-8fc49c343444.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundImage =
+          "url('https://snworksceo.imgix.net/dpn/7f265244-68c1-4483-b138-8fc49c343444.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B"; 
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
         break;
       case 'femrook':
-        document.querySelector('section.individual').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.9), rgba(238, 238, 238, 0.9)), url('https://snworksceo.imgix.net/dpn/85611786-a619-4496-9c5c-59ece6432fa5.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundImage =
+          "url('http://snworksceo.imgix.net/dpn/a53e770a-db89-47bc-8074-0a1a32807eda.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B"; 
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
         break;
       case 'femath':
-        document.querySelector('section.invidiual').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.8), rgba(238, 238, 238, 0.8)), url('https://snworksceo.imgix.net/dpn/68726eb4-7df0-4e26-9d83-3ad542d33846.sized-1000x1000.jpg')";
+        document.querySelector('section.invidiual').style.backgroundImage =
+          "url('https://snworksceo.imgix.net/dpn/68726eb4-7df0-4e26-9d83-3ad542d33846.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B";  
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
+        break;
+        case 'fbreak':
+        document.querySelector('section.individual').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/b4c3f932-9d69-41a1-b693-9a2b66846b1f.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B";  
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
+        break;
+      case 'come':
+        document.querySelector('section.individual').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/011b6265-19df-4f39-8342-84225d3aa94c.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B";  
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
+        break;
+      case 'mbreak':
+        document.querySelector('section.individual').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/c442c5db-3a57-4da0-9d08-4a3903d12f38.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundColor = "#011F5B";  
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
+        break;
+      case 'indchamp':
+        document.querySelector('section.invidiual').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/533c5f22-0469-42f5-bb7a-7989f291faa6.sized-1000x1000.jpg')";
+        document.querySelector('section.team').style.backgroundColor = "#011F5B";        
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
+        break;
+        case 'recbreak':
+        document.querySelector('section.invidiual').style.backgroundImage =
+          "url('https://snworksceo.imgix.net/dpn/68726eb4-7df0-4e26-9d83-3ad542d33846.sized-1000x1000.jpg')";
+        document.querySelector('section.individual').style.backgroundColor = "#011F5B";
+        document.querySelector('section.individual').style.backgroundBlendMode = "soft-light";
         break;
       default:
         document.querySelector('section.individual').style.background = '#EEE';
@@ -124,23 +168,31 @@ for (var i = 0; i < momTabs.length; i++) {
     // change department images
     switch (this.id) {
       case 'game':
-        document.querySelector('section.moment').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.9), rgba(238, 238, 238, 0.9)), url('https://snworksceo.imgix.net/dpn/5803b1e8-72a6-4810-9770-60123f4a066d.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/aa67d926-729c-46f0-9667-859e11ca195e.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundColor = "#990000";
+        document.querySelector('section.moment').style.backgroundBlendMode = "soft-light";
         break;
       case 'play':
-        document.querySelector('section.moment').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.8), rgba(238, 238, 238, 0.8)), url('https://snworksceo.imgix.net/dpn/7f265244-68c1-4483-b138-8fc49c343444.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/1f10ecfa-2405-4e9d-9fcf-fd5a96c74489.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundColor = "#990000";
+        document.querySelector('section.moment').style.backgroundBlendMode = "soft-light";
         break;
       case 'upset':
-        document.querySelector('section.moment').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.9), rgba(238, 238, 238, 0.9)), url('https://snworksceo.imgix.net/dpn/85611786-a619-4496-9c5c-59ece6432fa5.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/f15c5084-2dbc-4dc9-a8ae-f555ae2359eb.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundColor = "#990000";
+        document.querySelector('section.moment').style.backgroundBlendMode = "soft-light";
         break;
       case 'moment':
-        document.querySelector('section.moment').style.background =
-          "linear-gradient(rgba(238, 238, 238, 0.8), rgba(238, 238, 238, 0.8)), url('https://snworksceo.imgix.net/dpn/68726eb4-7df0-4e26-9d83-3ad542d33846.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundImage =
+          "url('https://s3.amazonaws.com/snwceomedia/dpn/f8abbc34-6199-4205-8bf0-179c1e288e5f.sized-1000x1000.jpg')";
+        document.querySelector('section.moment').style.backgroundColor = "#990000";
+        document.querySelector('section.moment').style.backgroundBlendMode = "soft-light";
         break;
       default:
-        document.querySelector('section.moment').style.background = '#CCC';
+        document.querySelector('section.moment').style.background = '#990000';
     }
 
     // make sure image covers background
