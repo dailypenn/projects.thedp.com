6819803d68bb2a947c1877b1ca1baa67ee7efa1c
Leah Wang <wanglihui17@gmail.com>
2019-03-17 17:03:36 -0400

    finished new bracket

--- 2019/03/penn-bracket/js/script.js
+++ 2019/03/penn-bracket/js/script.js
@@ -1,68 +1,68 @@
 var ltbracketData = {
   teams: [
     // SOCIAL
-    ['Chi Omega', 'Alpha Sig'],
-    ['Alpha Phi', 'Delta Sigma Pi'],
+    ['Alphi Phi', 'Chi Omega'],
+    ['Apes', 'OAX'],
 
-    ['Phi Gamma Nu', 'Delta Delta Delta'],
-    ['Zeta Tau Alpha', 'The Pennsylvania Punch Bowl'],
+    ['Penn Appetit', 'The Walk'],
+    ['SDT', 'ZBT'],
 
-    ['Penn Records', 'Penn Appetit'],
-    ['Kappa Alpha Theta', 'Lambda Chi Alpha'],
+    ['Tri Delt', 'Phi Delt'],
+    ['Delta Sigma Pi', 'ZTA'],
 
-    ['Sigma Nu', 'Alpha Iota Gamma'],
-    ['Sigma Kappa', 'Alpha Kappa Psi'],
+    ['Alpha Iota Gamma', 'ADPi'],
+    ['Alpha Kappa Psi', 'Friars'],
 
     // EDUCATIONAL
-    ['Penn Debate Society', 'Kite and Key'],
-    ['Government and Politics Association', 'Penn for Youth Debate'],
+    ['GPA', 'SWE'],
+    ['MERT', 'GBC'],
 
-    ['West Philadelphia Tutoring Project', 'Wharton Europe'],
-    ['MUSE', 'WQHS'],
+    ['Own It', 'Wharton Women'],
+    ['MUSE', "Class Boards ('19-'22)"],
 
-    ['The Undergraduate Assembly', 'Active Minds'],
-    ['Undergrad Sports Business Club', 'College Dean\'s Advisory Board'],
+    ['Penn Debate Society', 'Quiz Bowl'],
+    ['Wharton Europe', 'Wharton Asia Exchange'],
 
-    ['Wharton Undergrad Energy Group', 'Penn Aerospace Club'],
-    ['Wharton Undergrad Data Analytics Club', 'MERT']
+    ["Colege Dean's Adv. Board", 'KDSAP'],
+    ['Kite and Key', 'Petey Greene']
   ],
   results: [
     [
-      [634, 287],
-      [616, 278],
-      [486, 387],
-      [542, 361],
-      [243, 676],
-      [365, 495],
-      [529, 342],
-      [645, 281],
-      [275, 623],
-      [419, 417],
-      [804, 87],
-      [505, 360],
-      [430, 444],
-      [322, 516],
-      [218, 600],
-      [187, 705]
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ]
     ],[
-      [365, 454],
-      [213, 541],
-      [604, 160],
-      [292, 514],
-      [573, 191],
-      [530, 222],
-      [467, 258],
-      [213, 537]
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ]
     ],[
-      [286, 368],
-      [358, 308],
-      [275, 328],
-      [205, 377]
+      [, ],
+      [, ],
+      [, ],
+      [, ]
     ],[
-      [397, 215],
-      [296, 257]
+      [, ],
+      [, ]
     ], [
-      [386, 275]
+      [, ]
     ]
   ]
 };
@@ -70,78 +70,78 @@ var ltbracketData = {
 var rtbracketData = {
   teams: [
     // CULTURAL
-    ['Penn Hillel', 'Globemed'],
-    ['Vietnamese Students Assoc.', 'Onda Latina'],
+    ['Penn Dems', 'WQHS'],
+    ['Penn Hillel', 'Penn Dhamaka'],
 
-    ['PIPAC', 'Humans of UPenn'],
-    ['Penn Dems', 'Penn Fashion Collective'],
+    ['Onda Latina', 'Pan Asian Dance'],
+    ['PIPAC', 'Penn Masala'],
 
-    ['Women\'s Rowing', 'Women\'s Basketball'],
-    ['Club Swimming', 'Ski Club'],
+    ['W. Volleyball', 'Club Swimming'],
+    ['Humans of Penn', 'M. Basketball'],
 
-    ['Men\'s Club Basketball', 'Women\'s Volleyball'],
-    ['Men\'s Basketball', 'United Minorities Council'],
+    ['TAMID', 'V-Day'],
+    ['Penn Period Proj.', 'WPTP'],
 
     // PERFORMANCE
-    ['Shabbatones', 'Bloomers'],
-    ['West Philly Swingers', 'Mask and Wig'],
+    ['Penn Hype', 'Strictly Funk'],
+    ['Bloomers', 'Penn Dance'],
 
-    ['Penny Loafers', 'Soundworks Tap Factory'],
-    ['Penn Sirens', 'Theater Arts Committee'],
+    ['Dischord', 'Penny Loafers'],
+    ['Mask and Wig', 'PenNaach'],
 
-    ['Keynotes A Cappella', 'Penn Dance Company'],
-    ['Penn Masala', 'Penn Hype'],
+    ['Shabbatones', 'Soundsworks'],
+    ['Penn Sirens', 'Simply Chaos'],
 
-    ['Strictly Funk', 'Penn Dhamaka'],
-    ['V-Day', 'Pennaach']
+    ['West Philly Singers', 'Counterparts'],
+    ['TAC-e', 'Without a Net']
   ],
   results: [
     [
-      [547, 310],
-      [249, 602],
-      [113, 761],
-      [513, 366],
-      [392, 456],
-      [444, 419],
-      [207, 631],
-      [494, 357],
-      [214, 678],
-      [376, 523],
-      [506, 331],
-      [453, 382],
-      [368, 478],
-      [399, 463],
-      [527, 334],
-      [564, 303]
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ]
     ],[
-      [359, 394],
-      [448, 309],
-      [435, 303],
-      [373, 360],
-      [511, 282],
-      [402, 316],
-      [288, 454],
-      [426, 330]
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ],
+      [, ]
     ],[
-      [318, 255],
-      [295, 269],
-      [406, 189],
-      [263, 307]
+      [, ],
+      [, ],
+      [, ],
+      [, ]
     ],[
-      [331, 217],
-      [270, 283]
+      [, ],
+      [, ]
     ], [
-      [380, 297]
+      [, ]
     ]
   ]
 };
 
 var finalbracketData = {
   teams: [
-    ['Zeta Tau Alpha', 'Onda Latina']
+    [' ', ' ']
   ],
   results: [
-      [636, 589]
+      [ ,  ]
     ]
 };
 
