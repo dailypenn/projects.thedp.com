6b0faed1317e5744b642afec7a44449a2f3dde9f
peterbaile <peterchenwflms@gmail.com>
2020-08-20 00:38:04 -0400

    add CNAME and remove pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@peterbaile`,
   },
-  pathPrefix: '/projects.thedp.com',
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
