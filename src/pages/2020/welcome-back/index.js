809a75919612ce3318f85a625b1e02b1622ae80c
peterbaile <peterchenwflms@gmail.com>
2020-08-20 17:22:24 -0400

    resolve global styles issue

--- src/pages/2020/welcome-back/index.js
+++ src/pages/2020/welcome-back/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DFPSlotsProvider, AdSlot } from 'react-dfp'
 import s from 'styled-components'
+import { Helmet } from 'react-helmet'
 
 import SEO from "../../../components/seo"
 import NavBar from '../../../components/2020/welcome-back/Nav'
@@ -8,8 +9,6 @@ import WordWithLine from '../../../components/2020/welcome-back/WordWithLine'
 import UTB from '../../../components/2020/welcome-back/UTB'
 import Street from '../../../components/2020/welcome-back/Street'
 import Sports from '../../../components/2020/welcome-back/Sports'
-
-import './index.css'
 
 import { ROBOTO_REGULAR } from '../../../utils/font'
 
@@ -45,49 +44,54 @@ const MobileAd = s.div`
   }
 `
 
-const IndexPage = () => {
-  return (
-    <>
-      <SEO title="Home" />
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <Helmet>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Welcome Back | The Daily Pennsylvanian</title>
+      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+    </Helmet>
 
-      <NavBar />
+    <NavBar />
 
-      <WordWithLine word="NEWS" />
+    <WordWithLine word="NEWS" />
 
-      <DesktopAd>  
-        <DFPSlotsProvider dfpNetworkId="12234093">
-          <AdSlot adUnit="DP.com-Leaderboard" sizes={[[728, 90]]} />
-        </DFPSlotsProvider>
-      </DesktopAd>
-      <MobileAd>  
-        <DFPSlotsProvider dfpNetworkId="12234093">
-          <AdSlot adUnit="DP.com-mobile-leaderboard" sizes={[[320, 50]]} />
-        </DFPSlotsProvider>
-      </MobileAd>
+    <DesktopAd>  
+      <DFPSlotsProvider dfpNetworkId="12234093">
+        <AdSlot adUnit="DP.com-Leaderboard" sizes={[[728, 90]]} />
+      </DFPSlotsProvider>
+    </DesktopAd>
+    <MobileAd>  
+      <DFPSlotsProvider dfpNetworkId="12234093">
+        <AdSlot adUnit="DP.com-mobile-leaderboard" sizes={[[320, 50]]} />
+      </DFPSlotsProvider>
+    </MobileAd>
 
-      <WordWithLine word="OPINION" />
+    <WordWithLine word="OPINION" />
 
-      <Street />
+    <Street />
 
-      <DesktopAd>  
-        <DFPSlotsProvider dfpNetworkId="12234093">
-          <AdSlot adUnit="DP.com-Leaderboard" sizes={[[728, 90]]} />
-        </DFPSlotsProvider>
-      </DesktopAd>
-      <MobileAd>  
-        <DFPSlotsProvider dfpNetworkId="12234093">
-          <AdSlot adUnit="DP.com-mobile-leaderboard" sizes={[[320, 50]]} />
-        </DFPSlotsProvider>
-      </MobileAd>
+    <DesktopAd>  
+      <DFPSlotsProvider dfpNetworkId="12234093">
+        <AdSlot adUnit="DP.com-Leaderboard" sizes={[[728, 90]]} />
+      </DFPSlotsProvider>
+    </DesktopAd>
+    <MobileAd>  
+      <DFPSlotsProvider dfpNetworkId="12234093">
+        <AdSlot adUnit="DP.com-mobile-leaderboard" sizes={[[320, 50]]} />
+      </DFPSlotsProvider>
+    </MobileAd>
 
-      <WordWithLine word="SPORTS" />
-      <Sports />
+    <WordWithLine word="SPORTS" />
+    <Sports />
 
-      <UTB />
+    <UTB />
 
-      <Credit> Made with <span role="img"> 🎒</span> by The Daily Pennsylvanian © 2020. All rights reserved.  </Credit>
-    </>
-  )
-}
+    <Credit> Made with <span role="img"> 🎒</span> by The Daily Pennsylvanian © 2020. All rights reserved.  </Credit>
+  </>
+)
 
 export default IndexPage
