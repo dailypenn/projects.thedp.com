0e7aca6a89aa81ea5d757e4cd5ee824968a27770
peterbaile <peterchenwflms@gmail.com>
2020-08-20 20:39:37 -0400

    Updates

---
+++ welcome-back-script.js
@@ -0,0 +1,30 @@
+// The place I saw this effect:
+// https://angle2.agency/
+
+// Red Staper made a NICE tutorial about this effect! You should check it out!
+// https://www.youtube.com/watch?v=LgiadQQM6mo&t=5s
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.getElementsByClassName('spanSlow');
+const spansFast = document.getElementsByClassName('spanFast');
+
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  for (let i = 0; i < spansSlow.length; i++) {
+    spansSlow[i].style.transform = `translate(${speedSlow}px)`
+  }
+
+  for (let i = 0; i < spansFast.length; i++) {
+    spansFast[i].style.transform = `translate(${speedFast}px)`
+  }
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
