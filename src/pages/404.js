99d78d00119aa78f80f82d4ab6413d13befd1a6e
peterbaile <peterchenwflms@gmail.com>
2020-08-27 22:35:55 -0400

    add old project pages redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { navigate } from 'gatsby'
 
-import SEO from "../components/seo"
+// const NotFoundPage = () => (
+//   <>
+//     <SEO title="404: Not found" />
+//     <h1>NOT FOUND</h1>
+//     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+//   </>
+// )
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    const idx = window.location.href.split('/', 3).join('/').length
+    const slug = window.location.href.substring(idx)
+    navigate(`https://dailypenn.github.io/old.projects.thedp.com${slug}`)
+  }, [])
+
+  return null
+}
 
 export default NotFoundPage
