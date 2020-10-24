037d500f37db17a50e97e28ff3a54959b5162410
Calvin Hu <calvinhu73@gmail.com>
2020-10-24 01:40:18 -0400

    template for ExplainItToMe and InMyOpinion

--- src/components/2020/vote/ExplainItToMe.js
+++ src/components/2020/vote/ExplainItToMe.js
@@ -1,9 +1,78 @@
 import React from 'react'
 import s from 'styled-components'
 import Img from 'gatsby-image'
+import BackgroundImage from 'gatsby-background-image'
 
 import { Wrapper, WordWithLine } from './shared'
 import { StaticQuery, graphql } from 'gatsby'
+
+const Explainer = ({articles}) => (
+  <div className="col-md-4 pr-0">
+    <div class="border border-dark">
+      <Img fluid={articles[0].img.src.childImageSharp.fluid} />
+    </div>
+    <p className="text-center text-danger font-weight-bold pt-3"> EXPLAINER </p>
+    <p className="text-center" style={{fontSize: 28}}> {articles[0].title} </p>
+    <p className="text-center"> {articles[0].description} </p>
+    <p className="text-center text-uppercase"> BY {articles[0].author} </p>
+  </div>
+)
+
+const FeatureText = s.div`
+  padding-left: 9%;
+  padding-top: 12%;
+  color: #FFFFFF;
+  text-align: left;
+`
+const Feature = ({articles}) => (
+  <div className="col-md-8 pl-0">
+    <BackgroundImage className="h-100 border border-dark" fluid={articles[0].img.src.childImageSharp.fluid}>
+    <FeatureText>
+      <p className="text-danger font-weight-bold"> FEATURE </p>
+      <p className="w-50" style={{fontSize: 36}}> {articles[0].title} </p>
+      <p> {articles[0].description} </p>
+      <p className="pt-3 text-uppercase"> BY {articles[0].author} </p>
+    </FeatureText>
+    
+    
+    </BackgroundImage>
+    
+  </div>
+)
+
+const NewText = s.div`
+  padding-top: 25%;
+  text-align: center;
+`
+const BlueRedBar = s.div`
+  width: 15%;
+  height: 10px;
+  background-color: #D70000;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const New = ({articles}) => (
+  <div className="row">
+    <div className="col-md-4 pl-0">
+      <NewText>
+        <p className="text-danger font-weight-bold"> NEW! </p>
+        <p className="w-100" style={{fontSize: 36}}> {articles[0].title} </p>
+        
+    <BlueRedBar>
+      <div className="col w-50 h-100 px-0" style={{backgroundColor: "#2a5ad5"}}/>
+    </BlueRedBar>
+        <div className="d-flex justify-content-center">
+          <p className="w-75"> {articles[0].description} </p>
+        </div>
+        <p className="pt-3 text-uppercase"> BY {articles[0].author} </p>
+      </NewText>
+    </div>
+    <div className="col-md-8 pr-0">
+      <Img fluid={articles[0].img.src.childImageSharp.fluid} className="border border-dark" />
+    </div>
+  </div>
+)
 
 const ExplainItToMe = () => (
   <StaticQuery
@@ -35,14 +104,21 @@ const ExplainItToMe = () => (
     `}
     render={data => {
       const { node: { childrenVote2020ExplainItToMeJson: articles } } = data.allFile.edges[0]
-
+      console.log(articles)
       return (
         <Wrapper>
           <div id="explainers">
             <WordWithLine word="EXPLAIN IT TO ME" lineColor="#F05237"/>
-            <div>Explain it to me section here</div>
-            <Img fluid={articles[0].img.src.childImageSharp.fluid} />
-            <div>{articles[0].title}</div>
+              <div className="row">
+                <Feature articles={articles}/>
+                <Explainer articles={articles}/>
+              </div>
+
+              <New articles={articles}/>
+            
+
+
+
           </div>
         </Wrapper>
       )
--- src/components/2020/vote/InMyOpinion.js
+++ src/components/2020/vote/InMyOpinion.js
@@ -1,10 +1,94 @@
 import React from 'react'
 import s from 'styled-components'
 import Img from 'gatsby-image'
+import { RADIANT_REGULAR } from '../../../utils/font'
 
 import { Wrapper, WordWithLine } from './shared'
 import { StaticQuery, graphql } from 'gatsby'
 
+const H2 = s.h2` 
+  text-align: center; 
+  line-height: 0.1em;
+  margin-bottom: 3.2rem;
+  margin-top: 2rem;
+  font-size: 4em;
+  letter-spacing: -2px;
+  ${RADIANT_REGULAR}
+`
+const MustReadArticle = ({article}) => (
+  <div className="row pl-3 py-3">
+    <div className="col-4 py-3 border border-dark">
+      <Img fluid={article.img.src.childImageSharp.fluid} className="border border-dark" />
+    </div>
+    <div className="col-8">
+      <p style={{fontSize:20, lineHeight: 1}}>{article.title}</p>
+      <p className="text-uppercase">BY {article.author}</p>
+    </div>
+  </div>
+  
+)
+
+const MustRead = ({articles}) => (
+  <div className="col-md-4 pl-0" style={{backgroundColor: "#e4e4e4"}}>
+    <div className="d-flex justify-content-center">
+      <p className="pt-3">red and blue star here</p>
+    </div>
+    <H2> MUST READ: </H2>
+    <MustReadArticle article={articles[0]}/>
+    <MustReadArticle article={articles[0]}/>
+    <MustReadArticle article={articles[0]}/>
+    <MustReadArticle article={articles[0]}/>
+    <MustReadArticle article={articles[0]}/>
+    <div className="d-flex justify-content-center">
+      <p className="pt-3">red and blue star here</p>
+    </div>
+  </div>
+)
+const Editorial = ({articles}) => (
+  <div className="px-3">
+    <div className="row" style={{backgroundColor: "#e4e4e4"}}>
+      <div className="col-6 pl-5 pt-5">
+        <p className="text-danger font-weight-bold"> EDITORIAL </p>
+        <p className="w-100" style={{fontSize: 36, lineHeight: 1}}> {articles[0].title} </p>
+        <p style={{lineHeight: 1}}> {articles[0].description} </p>
+        <p className="text-uppercase"> BY {articles[0].author} </p>
+      </div>
+      <div className="col-6">
+        <Img fluid={articles[0].img.src.childImageSharp.fluid} className="border border-dark" />
+      </div>
+    </div>
+  </div>
+)
+
+const ExtraArticleImage = s.div`
+  height: 70%;
+  padding-top: 1rem;
+  padding-bottom: 1.5rem;
+`
+const ExtraArticle = ({article}) => (
+  <div className="col-6 text-center px-3">
+    <ExtraArticleImage>
+      <Img fluid={article.img.src.childImageSharp.fluid} className="border border-dark" />
+    </ExtraArticleImage>
+    <p style={{fontSize:20, lineHeight: 1}}>{article.title}</p>
+    <p className="text-uppercase">BY {article.author}</p>
+  </div>
+
+)
+
+const Extra = ({articles}) => (
+  <div>
+    <div className="row pt-3">
+      <ExtraArticle article={articles[0]}/>
+      <ExtraArticle article={articles[0]}/>
+    </div>
+    <div className="row pt-3">
+      <ExtraArticle article={articles[0]}/>
+      <ExtraArticle article={articles[0]}/>
+    </div>
+  </div>
+  
+)
 const InMyOpinion = () => (
   <StaticQuery
     query={graphql`
@@ -40,7 +124,14 @@ const InMyOpinion = () => (
         <Wrapper>
           <div id="opinion">
             <WordWithLine word="IN MY OPINION" lineColor="#F05237"/>
-            <div>In my opinion section here</div>
+            <div className="row">
+              <MustRead articles={articles}/>
+              <div className="col-md-1"></div>
+              <div className="col-md-7 px-0">
+                <Editorial articles={articles}/>
+                <Extra articles={articles}/>
+              </div>
+            </div>
           </div>
         </Wrapper>
       )
