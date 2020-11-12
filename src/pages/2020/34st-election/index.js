af51c72e4acf147cdcd18565f4b8ceb27e294479
Chirality <danieltaox@gmail.com>
2020-11-12 09:54:25 -0500

    update import paths in 34st-election

--- src/pages/2020/34st-election/index.js
+++ src/pages/2020/34st-election/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import s from 'styled-components'
-import { Helmet } from 'react-helmet'
+import React from "react"
+import s from "styled-components"
+import { Helmet } from "react-helmet"
 
-import Arts from 'src/components/2020/election/Arts.js'
-import Ego from 'src/components/2020/election/Ego.js'
-import Features from 'src/components/2020/election/Features.js'
-import HotTakes from 'src/components/2020/election/HotTakes.js'
-import WatchingListening from 'src/components/2020/election/WatchingListening.js'
+import Arts from "../../../components/2020/election/Arts.js"
+import Ego from "../../../components/2020/election/Ego.js"
+import Features from "../../../components/2020/election/Features.js"
+import HotTakes from "../../../components/2020/election/HotTakes.js"
+import WatchingListening from "../../../components/2020/election/WatchingListening.js"
 
-import ExplainItToMe from '../../../components/2020/vote/ExplainItToMe'
+import ExplainItToMe from "../../../components/2020/vote/ExplainItToMe"
 
-import { Navbar } from '../../../components/2020/election/Navbar'
+import { Navbar } from "../../../components/2020/election/Navbar"
 
 const Credit = s.div`
   color: #000000;
@@ -21,39 +21,84 @@ const Credit = s.div`
 `
 
 const IndexPage = () => {
-  return <>
-    <Helmet>
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Your guide for navigating this unprecedented virtual school year" />
-      <title>The Election | 34th Street Magazine</title>
-      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+  return (
+    <>
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Your guide for navigating this unprecedented virtual school year"
+        />
+        <title>The Election | 34th Street Magazine</title>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        />
 
-      <meta property="og:title" content="The Election | 34th Street Magazine" />
-      <meta property="og:image" content="https://snworksceo.imgix.net/dpn/1fe2ce10-46fa-402e-a9f2-498cc011769b.sized-1000x1000.jpg" />
-      <meta property="og:description" content="Your guide for navigating this unprecedented virtual school year" />
-      <meta property="og:type" content="article" />
-      <meta property="og:url" content="https://projects.thedp.com/2020/34st-election" />
+        <meta
+          property="og:title"
+          content="The Election | 34th Street Magazine"
+        />
+        <meta
+          property="og:image"
+          content="https://snworksceo.imgix.net/dpn/1fe2ce10-46fa-402e-a9f2-498cc011769b.sized-1000x1000.jpg"
+        />
+        <meta
+          property="og:description"
+          content="Your guide for navigating this unprecedented virtual school year"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:url"
+          content="https://projects.thedp.com/2020/34st-election"
+        />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="The Election | 34th Street Magazine" />
-      <meta name="twitter:image" content="https://snworksceo.imgix.net/dpn/1fe2ce10-46fa-402e-a9f2-498cc011769b.sized-1000x1000.jpg" />
-      <meta name="twitter:description" content="Your guide for navigating this unprecedented virtual school year" />
-      <meta name="twitter:url" content="https://projects.thedp.com/2020/34st-election" />
-      <meta name="twitter:site" content="@dailypenn"></meta>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="The Election | 34th Street Magazine"
+        />
+        <meta
+          name="twitter:image"
+          content="https://snworksceo.imgix.net/dpn/1fe2ce10-46fa-402e-a9f2-498cc011769b.sized-1000x1000.jpg"
+        />
+        <meta
+          name="twitter:description"
+          content="Your guide for navigating this unprecedented virtual school year"
+        />
+        <meta
+          name="twitter:url"
+          content="https://projects.thedp.com/2020/34st-election"
+        />
+        <meta name="twitter:site" content="@dailypenn"></meta>
 
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
-    </Helmet>
-    <Navbar />
-    <Features />
-    <Ego />
-    <HotTakes />
-    <Arts />
-    <WatchingListening />
-  </>
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossOrigin="anonymous"
+        ></script>
+      </Helmet>
+      <Navbar />
+      <Features />
+      <Ego />
+      <HotTakes />
+      <Arts />
+      <WatchingListening />
+    </>
+  )
 }
 
 export default IndexPage
