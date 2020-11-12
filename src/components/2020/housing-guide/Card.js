d26db846e9f84b1e5a2a7cb96ceca42cbf2040ba
Chirality <danieltaox@gmail.com>
2020-11-11 23:59:30 -0500

    add flush display option for Card component

--- src/components/2020/housing-guide/Card.js
+++ src/components/2020/housing-guide/Card.js
@@ -4,19 +4,21 @@ import s from 'styled-components';
 const WHITE = `#FFFFFF`;
 const BOX_SHADOW = `0px 5px 20px rgba(0, 0, 0, 0.1)`;
 
-export const CardWrapper = s.div`
-    ${({ padding}) => padding && `padding: 40px 40px 30px 40px;`}
+export const CardWrapper = s.div.attrs(({ className }) => ({
+  className
+}))`
     background-color: ${WHITE};
     border-radius: 15px;
     box-shadow: ${BOX_SHADOW};
     display: flex;
     justify-content: center;
     flex-direction: column;
-    margin-bottom: 60px;
+  ${({ flush }) => flush || `padding-bottom: 30px;`}
     transition: all 0.3s;
-    padding-bottom: 30px;
+  margin-bottom: 60px;
+  overflow: hidden;
 `
 
-export const Card = ({ children, padding }) => <CardWrapper padding={padding}>
+export const Card = ({ className, children, flush }) => <CardWrapper className={className} flush={flush}>
     {children}
 </CardWrapper>
