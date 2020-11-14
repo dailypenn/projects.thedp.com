ab96f144cfdd6e0fd500b68b00bfce448adddcf9
Chirality <danieltaox@gmail.com>
2020-11-13 20:59:57 -0500

    complete Features section

--- src/components/2020/election/Features.js
+++ src/components/2020/election/Features.js
@@ -1,92 +1,88 @@
 import React from "react"
 import s from 'styled-components'
-import { TemporarySectionHeader } from "./shared"
-import { FUTURA_REGULAR} from '../../../utils/font'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import { Container, Row, Col } from 'react-bootstrap'
 
+import { SectionHeader, Title, Author } from "./shared"
+import { CANELA_REGULAR, FUTURA_REGULAR } from '../../../utils/font'
 
-const ArticleGrid = s.div`
-    display: grid;
-    grid-template-columns: 35% 60%;
-    grid-gap: 3rem;
-    height: 22rem;
-    width: 63%;
-    margin: auto;
-    font-family: 'Canela', 'sans-serif';
-`
-const LeftTitle= s.p`
-    color: black;
-    text-align: center;
-    margin-top: 1.4rem;
-    font-size: 1.1em;
-    line-height: 1.1;
-    font-weight: light;
+const FeatureWrapper = s.div`
+  position: absolute;
+  top: 0;
+  text-align: right;
+  width: 90%;
+  height: 100%;
 `
 
-const LeftAuthor = s.p`
-    ${FUTURA_REGULAR}
-    color: black;
-    text-align: center;
-    margin-top: 1.4rem;
-    font-size: 0.7em;
-    font-weight: light;
+// TODO: ADD DIFFERENT SIZING AND POSITION FOR DIFFERENT BREAKPOINTS
+const FeatureTitle = s.div`
+  ${CANELA_REGULAR}
+  position: absolute;
+  color: white;
+  top: 20%;
+  right: 10%;
+  font-size: 2em;
+  line-height: 1.1;
+  text-align: right;
+  width: 60%;
 `
 
-const RightTitle= s.div`
-    align-self: right;
-    margin-right: 2%;
-    text-align: center;
-    display: inline-block;
-    color: white;
-    margin-top: 15%;
-    font-size: 2.1vw;
-    width: 60%;
-    font-weight: 100;
+const FeatureAuthor = s.div`
+  ${FUTURA_REGULAR}
+  position: absolute;
+  right: 10%;
+  color: white;
+  top: 60%;
+  font-size: 0.7em;
+  font-weight: light;
 `
-const RightDiv = s.div`
-    position: relative;
-    text-align: right; 
-    width: 100%;
-    background-image: url("/img/34st-election/politician.png");
-    background-size: contain;
-    background-repeat: no-repeat;
-    line-height: 1;
-`
-const RightAuthor = s.div`
-    ${FUTURA_REGULAR}
-    color: white;
-    align-self: right;
-    margin-right: 10%;
-    margin-top: 4%;
-    text-align: right;
-    display: inline-block;
-    font-size: 0.75vw;
-    width: 60%;
-    font-weight: 100;
-`
-const Features = () => (
-    <div>
-        <TemporarySectionHeader>FEATURES</TemporarySectionHeader>
-        <ArticleGrid>
-            <div>
-                <img
-                    src="/img/34st-election/ballot.png"
-                    className="img-fluid"
-                    
-                />
-                <LeftTitle>
-                    <span style={{"font-weight": "bolder"}}>There's a limit to what can be expressed through the voting process</span>, but civic engagement doesn't end on Election Day.
-                </LeftTitle>
-                <LeftAuthor>
-                    BY PEARL LIU
-                </LeftAuthor>
-            </div>
-            <RightDiv>
-                <RightTitle>In the wake of a Trump loss, the future of the Republican Party lies in the hands of some of its youngest members.</RightTitle>
-                <RightAuthor>BY DELANI SAGNER</RightAuthor>
-            </RightDiv>
-        </ArticleGrid>
-    </div>
-)
+
+const Features = () => {
+  const { left, right } = useStaticQuery(graphql`
+    query {
+      left: file(relativePath: {eq: "engagement.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      right: file(relativePath: {eq: "republican.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return <>
+    <SectionHeader>FEATURES</SectionHeader>
+    <Container>
+      <Row>
+        <Col md={5}>
+          <Img fluid={left.childImageSharp.fluid} />
+          <Title>
+            <b>There's a limit to what can be expressed through the voting process</b>, but civic engagement doesn't end on Election Day.
+          </Title>
+          <Author>
+            BY PEARL LIU
+          </Author>
+        </Col>
+        <Col md={7}>
+          <Img fluid={right.childImageSharp.fluid} style={{ filter: `brightness(.7)` }} />
+          <FeatureWrapper>
+            <FeatureTitle>In the wake of a Trump loss, the future of the Republican Party lies in the hands of some of its youngest members.</FeatureTitle>
+            <FeatureAuthor>BY DELANI SAGNER</FeatureAuthor>
+          </FeatureWrapper>
+        </Col>
+      </Row>
+      </Container>
+    {/* TODO: ADD AD HERE */}
+  </>
+}
 
 
 export default Features
