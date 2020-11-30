1dba7e1067523ae4a1571f5be23dceed4cd84b3d
nmotya <nmotya@sas.upenn.edu>
2020-11-30 11:10:20 -0500

    progress on always there

--- src/components/2020/giving-tuesday/AlwaysThere.js
+++ src/components/2020/giving-tuesday/AlwaysThere.js
@@ -1,16 +1,83 @@
 import React from 'react'
 import s from 'styled-components'
-
-import { Header, WHITE, PINK } from './shared'
+import { Col, Row } from 'react-bootstrap'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Header, WHITE, BLACK, YELLOW, PINK, CenteredButton } from './shared'
+import { RowWithPadding } from '../../shared'
+import Img from 'gatsby-image'
 
 const Wrapper = s.div`
   background-color: ${PINK};
+  padding-bottom: 5rem;
+`
+const ImagesDiv = s.div`
+  margin-top: 1vw;
+`
+const SpacedRow = s(Row)`
+  margin-top: 1vw;
+`
+const ParaDiv = s.div`
+  margin-top: 10vw;
+  margin-left: 4vw;
+  font-family: 'Gopher', sans-serif;
 `
 
-const AlwaysThere = () => (
-  <Wrapper>
-    <Header title="always there" color={WHITE} />
-  </Wrapper>
-)
+const AlwaysThere = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allFile(filter: { relativePath: { eq: "decades_GT_2020.json" } }) {
+      edges {
+        node {
+          childrenDecadesGt2020Json {
+            image {
+              src {
+                childImageSharp {
+                  fluid(maxWidth: 600, maxHeight: 800) {
+                    ...GatsbyImageSharpFluid
+                    src
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`)
+
+const {
+  node: { childrenDecadesGt2020Json: stories },
+} = data.allFile.edges[0]
+
+  return(
+    <>
+      <Wrapper>
+        <Header title="ALWAYS THERE" titleColor={WHITE} subColor={YELLOW} />
+        <Row>
+          <Col sm={12} md={7}>
+            <ImagesDiv>
+              <RowWithPadding padding="3" >
+                {stories.map((story) => (
+                  <Col sm={12} md={4}>
+                    <Img fluid={story.image.src.childImageSharp.fluid} className="img-fluid" style={{"margin-top": "1vw"}} />
+                  </Col>
+                ))}
+              </RowWithPadding>
+            </ImagesDiv>
+          </Col> 
+          <Col sm={12} md={4}>
+            <ParaDiv>
+                  <h4>THROUGH THE DECADES</h4>
+                  <p style = {{color: WHITE}}>UPenn students have always been at the forefront of social justice issues and its solutions, and wherever it happens, the DP is there to spotlight extraordinary causes and individuals to spread awareness and to bring to our attention important events that demand to be noticed.</p>
+                  <p style = {{color: WHITE}}>From uncovering unjust surveillance, participating in sit-ins, and protesting police brutality, student activism has evolved tremendously over the years and continues to be one of the Quakers community’s most distinctive characteristics. The DP evolves just as our community does, inspired to keep up with its achievements and dedicated to sharing them to the wider world.</p>
+                  <CenteredButton text="donate" bgColor={BLACK} textColor={WHITE} posLeft width="30" />
+            </ParaDiv>
+          </Col>
+        </Row>
+      </Wrapper>
+    </>  
+  )
+}
 
 export default AlwaysThere
