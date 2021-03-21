03f8020db529f310590eef12bad7f86a3ba87069
Raunaq Singh <raunaqsingh2020@gmail.com>
2021-03-21 06:13:44 -0400

    Updates

--- component---src-pages-2020-nec-cb-23-js-10bb24029a9ca3f7740b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{svUu:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),r=t.n(a),c=t("Wbzz"),u=t("g9Kp");n.default=function(){return r.a.createElement(c.StaticQuery,{query:"616067709",render:function(e){return r.a.createElement(u.a,{data:e.allFile.nodes[0].childNecJson})}})}}}]);
-//# sourceMappingURL=component---src-pages-2020-nec-cb-23-js-10bb24029a9ca3f7740b.js.map
