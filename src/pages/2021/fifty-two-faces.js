0dc8541ec835f38fea07101dbed8f1dc06ec7f4e
jonahcharlton <66034422+jonahcharlton@users.noreply.github.com>
2021-03-22 00:21:48 -0400

    Update fifty-two-faces.js
    photos

--- src/pages/2021/fifty-two-faces.js
+++ src/pages/2021/fifty-two-faces.js
@@ -120,67 +120,67 @@ const IndexPage = () => {
           <Quotes section={section1} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={"https://snworksceo.imgix.net/dpn/16294047-a025-4359-8d48-9356bc14b8c7.sized-1000x1000.jpg?"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/f326fbd1-aead-46b7-9bae-ed294b0bb4bd"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes2">
           <Quotes section={section2} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/0d53f58f-8653-4367-873e-99b636c1c760"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes3">
           <Quotes section={section3} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/52f609e9-728c-4f04-be8f-fc4c2aafca45"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes4">
           <Quotes section={section4} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/5559c169-f6a9-448c-980d-949a6591d82a"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes5">
           <Quotes section={section5} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/05cc7f07-9ad6-4254-b2b9-bd466db67e68"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes6">
           <Quotes section={section6} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/80f8d486-36dc-4262-b4c5-dfc4f75f55a4"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes7">
           <Quotes section={section7} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/e195aeec-0188-4a8f-a99c-85f760f7082d"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes8">
           <Quotes section={section8} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/f0a3f4ae-0a99-42e2-94f6-4d806a7162e1"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes9">
           <Quotes section={section9} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/c950a79a-caae-49f5-aa00-04a0cc794ebd"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes10">
           <Quotes section={section10} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/c1864d3b-c3a7-45c5-bb6c-00788910220f"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes11">
           <Quotes section={section11} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
         </div>
         <div>
-          <img src={FiftyTwoFacesCoverImg} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
+          <img src={"https://dpn.ceo.getsnworks.com/ceo/content/198ed8f7-91db-4be1-afb2-ebdb59d3d71c"} style={{ width: '100%', boxShadow: '0px 0px 7px 7px #00000029', }} />
         </div>
         <div id="quotes12">
           <Quotes section={section12} bgColor="#ffffff" textColor="#171717" dashColor="#bf1919"/>
