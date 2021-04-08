c58c2be45b9801af8e7bc6306ee96e93e10d506b
Raunaq Singh <raunaqsingh2020@gmail.com>
2021-04-07 23:54:34 -0400

    start nec 2021

---
+++ src/pages/2021/NEC/cb21.js
@@ -0,0 +1,194 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Section from '../../../components/2020/NEC/Section'
+
+const Index = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allFile(filter: { relativePath: { eq: "cb21_2020.json" } }) {
+          nodes {
+            childNecJson {
+              President {
+                campaign_website
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                photo
+                youtube_video_url
+              }
+              Executive_VP {
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                name
+                introduction
+                platform_points
+                youtube_video_url
+              }
+              VP_External {
+                facebook_url
+                image_name
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                youtube_video_url
+              }
+              VP_Internal {
+                campaign_website
+                facebook_url
+                image_name
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                youtube_video_url
+                platform_points
+              }
+              VP_Finance {
+                facebook_url
+                image_name
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                youtube_video_url
+              }
+              College_Chair {
+                facebook_url
+                image_name
+                introduction
+                instagram_handle
+                name
+                platform_points
+                statement
+                youtube_video_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+              }
+              Engineering_Chair {
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                statement
+              }
+              Nursing_Chair {
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                statement
+                youtube_video_url
+              }
+              Wharton_Chair {
+                campaign_website
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                instagram_handle
+                name
+                platform_points
+                youtube_video_url
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Section data={data.allFile.nodes[0].childNecJson} />}
+  />
+)
+
+export default Index
---
+++ src/pages/2021/NEC/index.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Section from '../../../components/2020/NEC/Section'
+
+const Index = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allFile(filter: { relativePath: { eq: "ua_2020.json" } }) {
+          nodes {
+            childNecJson {
+              Undergraduate_Assembly_President {
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                youtube_video_url
+              }
+              Undergraduate_Assembly_VP {
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                youtube_video_url
+              }
+              College_of_Arts___Sciences_Representative {
+                campaign_website
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                instagram_handle
+                name
+                platform_points
+                youtube_video_url
+              }
+              Wharton_Representative {
+                campaign_website
+                facebook_url
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                introduction
+                name
+                platform_points
+                youtube_video_url
+              }
+              SEAS_Representative {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              Nursing_Representative {
+                facebook_url
+                introduction
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                instagram_handle
+                name
+                platform_points
+                statement
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Section data={data.allFile.nodes[0].childNecJson} />}
+  />
+)
+
+export default Index
