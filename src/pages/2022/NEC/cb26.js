75ca1e18b41ee213c7f3aa3920a7cfe06cec6882
Raunaq Singh <raunaqsingh2020@gmail.com>
2022-09-16 02:59:46 -0400

    update nec center for f22

---
+++ src/pages/2022/NEC/cb26.js
@@ -0,0 +1,157 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Section from '../../../components/2022/nec/Section'
+
+const Index = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allFile(filter: { relativePath: { eq: "nec22f_cb_26.json" } }) {
+          nodes {
+            childNecJson {
+              President {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              Executive_VP {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              VP_of_External_Affairs {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              VP_of_Internal_Affairs {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              VP_of_Finance {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              College_Class_Chair {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              Wharton_Class_Chair {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              Engineering_Class_Chair {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+              Nursing_Class_Chair {
+                image {
+                  src {
+                    childImageSharp {
+                      fluid(maxWidth: 400, maxHeight: 400) {
+                        ...GatsbyImageSharpFluid
+                        src
+                      }
+                    }
+                  }
+                }
+                introduction
+                name
+                platform_points
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Section data={data.allFile.nodes[0].childNecJson} />}
+  />
+)
+
+export default Index
--- src/pages/2022/NEC/index.js
+++ src/pages/2022/NEC/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Section from '../../../components/2022/nec-spring/Section'
+import Section from '../../../components/2022/nec/Section'
 
 const Index = () => (
   <StaticQuery
     query={graphql`
       query {
-        allFile(filter: { relativePath: { eq: "nec22s_ua.json" } }) {
+        allFile(filter: { relativePath: { eq: "nec22f_ua.json" } }) {
           nodes {
             childNecJson {
-              Undergraduate_Assembly_President {
-                facebook_url
+              New_Student_Representative {
                 image {
                   src {
                     childImageSharp {
@@ -22,101 +21,9 @@ const Index = () => (
                     }
                   }
                 }
-                instagram_handle
                 introduction
                 name
                 platform_points
-                campaign_website
-              }
-              Undergraduate_Assembly_VP {
-                facebook_url
-                image {
-                  src {
-                    childImageSharp {
-                      fluid(maxWidth: 400, maxHeight: 400) {
-                        ...GatsbyImageSharpFluid
-                        src
-                      }
-                    }
-                  }
-                }
-                instagram_handle
-                introduction
-                name
-                platform_points
-                campaign_website
-              }
-              College_of_Arts___Sciences_Representative {
-                facebook_url
-                image {
-                  src {
-                    childImageSharp {
-                      fluid(maxWidth: 400, maxHeight: 400) {
-                        ...GatsbyImageSharpFluid
-                        src
-                      }
-                    }
-                  }
-                }
-                instagram_handle
-                introduction
-                name
-                platform_points
-                campaign_website
-              }
-              Wharton_Representative {
-                facebook_url
-                image {
-                  src {
-                    childImageSharp {
-                      fluid(maxWidth: 400, maxHeight: 400) {
-                        ...GatsbyImageSharpFluid
-                        src
-                      }
-                    }
-                  }
-                }
-                instagram_handle
-                introduction
-                name
-                platform_points
-                campaign_website
-              }
-              SEAS_Representative {
-                facebook_url
-                image {
-                  src {
-                    childImageSharp {
-                      fluid(maxWidth: 400, maxHeight: 400) {
-                        ...GatsbyImageSharpFluid
-                        src
-                      }
-                    }
-                  }
-                }
-                instagram_handle
-                introduction
-                name
-                platform_points
-                campaign_website
-              }
-              Nursing_Representative {
-                facebook_url
-                image {
-                  src {
-                    childImageSharp {
-                      fluid(maxWidth: 400, maxHeight: 400) {
-                        ...GatsbyImageSharpFluid
-                        src
-                      }
-                    }
-                  }
-                }
-                instagram_handle
-                introduction
-                name
-                platform_points
-                campaign_website
               }
             }
           }
